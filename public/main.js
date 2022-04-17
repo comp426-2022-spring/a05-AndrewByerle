aae30a6f95bb4f8982289948ee613d4518e223f7
Andrew Byerle <andran@live.unc.edu>
2022-04-17 19:14:34 -0400

    implemented flip one coin

--- public/main.js
+++ public/main.js
@@ -6,3 +6,32 @@
 // Enter number and press button to activate coin flip series
 
 // Guess a flip by clicking either heads or tails button
+
+
+// Event listener for whatever is being clicked 
+document.addEventListener("click", activeNow);
+// Replace text in anything with "active" id
+function activeNow() {
+    const active_now = document.activeElement
+    document.getElementById("active").innerHTML = active_now;
+    console.log(active_now)
+}
+
+
+const coin = document.getElementById("singlenav")
+// Add event listener for coin button
+coin.addEventListener("click", flipCoin)
+function flipCoin() {
+    fetch('http://localhost:5555/app/flip/', { mode: 'cors' })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (result) {
+            console.log(result);
+            document.getElementById("result").innerHTML = result.flip;
+            document.getElementById("quarter").setAttribute("src", "./assets/img/" + result.flip + ".png");
+            // coin.disabled = true
+        })
+}
+
+
